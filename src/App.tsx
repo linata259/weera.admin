d787b13ca8feea9fd17455727c1ac450ad6e3036
Naomi Lagat <naomiejerutoe@gmail.com>
2026-05-19 13:21:47 +0300

    jobs

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import AdminLayout from './layouts/AdminLayout';
+import Jobs from './features/jobs/pages/jobs';
 
 export default function App() {
   return (
     <BrowserRouter>
       <AdminLayout>
         <Routes>
-          <Route path="/"          element={<Navigate to="/dashboard" replace />} />
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
           <Route path="/dashboard" element={<div>Dashboard Page</div>} />
-          <Route path="/users"     element={<div>User Management</div>} />
-          <Route path="/jobs"     element={<div>Job Management</div>} />
+          <Route path="/users" element={<div>User Management</div>} />
+          <Route path="/jobs" element={<Jobs />} />
         </Routes>
       </AdminLayout>
     </BrowserRouter>
---
+++ src/features/jobs/pages/jobs.tsx
@@ -0,0 +1,21 @@
+
+import React from 'react';
+
+
+const JobPage: React.FC = () => {
+    return (
+    
+        <div style={{ padding: 24 }}>
+            <div style={{
+            padding: 24,
+            borderRadius: 6
+            }}>
+            <h2 style={{ margin: 0, fontSize: 28, color: '#92400e' }}>Job Management</h2>
+            <p style={{ marginTop: 8, color: '#92400e' }}>Here are some charts and stats.</p>
+            </div>
+        </div>
+    
+    );
+};
+
+export default JobPage;
